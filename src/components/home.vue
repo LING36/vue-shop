76c4dc4d154fab0709886966e9de19f7b0e57353
LING36 <1wling123@sina.cn>
2020-10-17 23:59:00 +0800

    首页-整体布局

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="">
-      <el-button type="info" @click="logout">退出登录</el-button>
+  <div class="home-container">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+    <el-button type="info" @click="logout">退出登录</el-button>
   </div>
 </template>
 
@@ -27,4 +34,27 @@ export default {
 }
 </script>
 <style lang='less' scoped>
+.home-container, .el-container{
+  height: 100%;
+}
+.el-header{
+  background-color: #373d37;
+  color: #fff;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #333744;
+  color: #fff;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #eaedfe;
+  color: #fff;
+  text-align: center;
+  line-height: 160px;
+}
 </style>
